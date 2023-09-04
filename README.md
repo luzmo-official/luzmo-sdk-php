b532418414cd1f31a3ca5513ab3534ee73455b59
Paresh Chouhan <paresh@cumul.io>
2023-09-04 20:11:56 +0530

    Chore: Luzmo rebranding

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-# Cumul.io API
+# Luzmo API
 
-You can use this PHP project to interact with the [Cumul.io](https://cumul.io) API in order to create, modify or delete datasets, dashboards or push new data into the platform in a programmatic way.
+You can use this PHP project to interact with the [Luzmo](https://luzmo.com) API in order to create, modify or delete datasets, dashboards or push new data into the platform in a programmatic way.
 
 ## Installation
 Install the latest version with
-`composer require cumulio/cumul.io-sdk-php`
+`composer require luzmo/luzmo-sdk-php`
 
 ## Usage
-Include the `cumulio` php package in your project by including the composer `vendor/autoload.php` file.
+Include the `luzmo` php package in your project by including the composer `vendor/autoload.php` file.
 See the `example.php` file for examples of how to create datasets or push data into the platform (triggering real-time dashboard updates).
 See the `example-embedding.php` file to see an example of how to use the API to securely embed dashboards in a web page (with serverside pre-filtering of the data that the end-user can query).
 
 ## Documentation
 
-The API documentation (available services and methods) can be found [here](https://developer.cumul.io/?php#introduction).
+The API documentation (available services and methods) can be found [here](https://developer.luzmo.com/?php#introduction).
--- example-embedding.php
+++ example-embedding.php
@@ -1,11 +1,11 @@
 <?php
 // This code should be executed server-side. Your API key and token should be kept confidential.
 require 'vendor/autoload.php';
-use Cumulio\Cumulio;
+use Luzmo\Luzmo;
 
-// Connect to Cumul.io API
-$client = Cumulio::initialize('< Your API key >', '< Your API token >'); // Fill in your API key & token
-// Set third, optional property to https://api.cumul.io/ (default, EU multitenant env), https://api.us.cumul.io (US multitenant env) or your specific VPC address
+// Connect to Luzmo API
+$client = Luzmo::initialize('< Your API key >', '< Your API token >'); // Fill in your API key & token
+// Set third, optional property to https://api.luzmo.com/ (default, EU multitenant env), https://api.us.luzmo.com (US multitenant env) or your specific VPC address
 
 // On page requests of pages containing embedded dashboards, request an "authorization"
 $integrationId = 'b9a0c66e-2986-4b0f-913f-af54d9132453'; // Fill in your integration ID
@@ -32,22 +32,22 @@ $authorization = $client->create('authorization', array(
 <html>
   <head>
     <meta charset="UTF-8">
-    <title>Cumul.io embedding example</title>
+    <title>Luzmo embedding example</title>
   </head>
   <body style="font-family: sans-serif;">
     <div style="margin-left: 28px">
-      <h1 style="font-weight: 200;">Cumul.io embedding example</h1>
-      <p>This page contains an example of an embedded dashboard of Cumul.io. The dashboard data is securely filtered server-side, so clients can only access data to which your application explicitly grants access (in this case, the data of client_id = 1234).</p>
+      <h1 style="font-weight: 200;">Luzmo.com embedding example</h1>
+      <p>This page contains an example of an embedded dashboard of Luzmo.com. The dashboard data is securely filtered server-side, so clients can only access data to which your application explicitly grants access (in this case, the data of client_id = 1234).</p>
       <p>Try to resize your page to see the dashboard adapting to different screen modes.</p>
     </div>
-    <cumulio-dashboard
-        appServer="https://app.cumul.io/"> 
-        <!-- Set appServer to https://app.cumul.io/ (default, EU multitenant env), https://app.us.cumul.io (US multitenant env) or your specific VPC address -->
-    </cumulio-dashboard>
+    <luzmo-dashboard
+        appServer="https://app.luzmo.com/"> 
+        <!-- Set appServer to https://app.luzmo.com/ (default, EU multitenant env), https://app.us.luzmo.com (US multitenant env) or your specific VPC address -->
+    </luzmo-dashboard>
     <!-- Check out the latest version on our npm page, as well as our components for frameworks such as react, vue and angular -->
-    <script src="https://cdn-a.cumul.io/js/cumulio-dashboard/1.0.7/cumulio-dashboard.min.js" charset="utf-8"></script>
+    <script src="https://cdn.luzmo.com/js/luzmo-embed/5.0.0/luzmo-embed.min.js" charset="utf-8"></script>
     <script type="text/javascript">
-      const dashboardElement = document.querySelector('cumulio-dashboard');
+      const dashboardElement = document.querySelector('luzmo-dashboard');
       // We can now set the key and token to the dashboard component.
       dashboardElement.authKey = '<?php echo $authorization['id']; ?>'
       dashboardElement.authToken ='<?php echo $authorization['token']; ?>'
